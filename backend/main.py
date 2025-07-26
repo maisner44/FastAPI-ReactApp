b6e1325e65b8bb4895ab122000512a7ae5d9ceab
Yehor Hashkodor <yehor.hashkodor@develops.today>
2025-07-26 14:51:04 +0300

    add config and setup db models

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,23 @@
 from fastapi import FastAPI
-print('asda')
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
+
+app = FastAPI(
+    tittle="Choose your own adventure Game API",
+    description="api to generate cool stories",
+    version="0.1.0",
+    dosc_url="/docs",
+    redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
